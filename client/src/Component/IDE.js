c2c5841fe0040fa650cdd50334ca5117806a909d
xiaozhen <xli851@aucklanduni.ac.nz>
2023-01-04 22:05:55 +1300

    Added code running area

--- client/src/Component/IDE.js
+++ client/src/Component/IDE.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import Editor from '@monaco-editor/react'
 import { useRef } from 'react'
+import CodeRunArea from "./CodeRunArea";
 
-const IDE = (props) => {
+const IDE = () => {
     const editorRef = useRef(null);
 
     const handleEditorDidMount = (editor, monaco) => {
         editorRef.current = editor;
     }
-    return (
 
+    const getEditorValue = () => {
+        console.log('get value', editorRef.current.getValue())
+        return editorRef.current.getValue();
+    }
+
+    return (
         <div>
             <Editor
               height='400px'
               theme='vs-bright'
-              defaultLanguage='markdown'
+              defaultLanguage='python3'
               onMount={handleEditorDidMount}
-              value = {props.msg}
             />
+            <CodeRunArea getSourceCode={getEditorValue} className="code-run-area" />
         </div>
-  )
+    )
 }
 
 export default IDE
